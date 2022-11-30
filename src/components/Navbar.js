8339a62b1021c743899c75368555fbb709851d12
Gianni <gianniicherry@gmail.com>
2022-11-29 19:09:16 -0800

    Full working navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,51 @@
-import React from "react";
-import { NavbarContainer } from "../styles/navbar.styles.js"
+import React , {useState} from "react";
+import { 
+    NavbarContainer,
+    LeftContainer,
+    RightContainer,
+    NavbarInnerContainer,
+    NavbarExtendedContainer,
+    NavbarLinkContainer,
+    NavbarLink,
+    LogoImg,
+    OpenLinksButton,
+    NavbarLinkExtended,
+ } from "../styles/navbar.styles.js"
+ import logo from "../assets/logo.png"
 
 function Navbar(){
-    return <NavbarContainer></NavbarContainer>
-}
+    const [extendNavbar, setExtendNavbar] = useState(false)
+
+    function handleExtendNavBar(){
+        setExtendNavbar((extendNavbar)=> !extendNavbar)
+    }
+
+
+    return (
+    <NavbarContainer extendNavbar={extendNavbar}>
+        <NavbarInnerContainer>
+        <LeftContainer>
+            <NavbarLinkContainer>
+                <NavbarLink to="/">Home</NavbarLink>
+                <NavbarLink to="/ItemForm">Sell</NavbarLink>
+                <NavbarLink to="/ItemList">Bid</NavbarLink>
+                <OpenLinksButton 
+                onClick={handleExtendNavBar}>{extendNavbar ? <> &#10005;</> : <>&#8801;</>}
+                </OpenLinksButton>
+            </NavbarLinkContainer>
+        </LeftContainer>
+        <RightContainer>
+            <LogoImg src={logo}></LogoImg>
+        </RightContainer>
+        </NavbarInnerContainer>
+       {extendNavbar && (
+           <NavbarExtendedContainer>
+                <NavbarLinkExtended to="/">Home</NavbarLinkExtended>
+                <NavbarLinkExtended to="/ItemForm">Sell</NavbarLinkExtended>
+                <NavbarLinkExtended to="/ItemList">Bid</NavbarLinkExtended>
+        </NavbarExtendedContainer>
+       )}       
+    </NavbarContainer>
+)}
 
 export default Navbar
